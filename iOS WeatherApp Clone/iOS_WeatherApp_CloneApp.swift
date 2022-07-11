1f1ec381dda7c44f1168c4b0c6b7a8805e5e72bf
RezEnayati <94985316+RezEnayati@users.noreply.github.com>
2022-07-11 13:35:40 -0400

    Initial Commit

---
+++ iOS WeatherApp Clone/iOS_WeatherApp_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_WeatherApp_CloneApp.swift
+//  iOS WeatherApp Clone
+//
+//  Created by Reza Enayati on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_WeatherApp_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
